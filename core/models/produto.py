1a948884b28c46f5430742291c8a5cfe32507f7c
fujulia <juliafu713@gmail.com>
2025-06-10 16:59:38 -0300

    Feat: Criação da tabela tamanho #4

--- core/models/produto.py
+++ core/models/produto.py
@@ -1,16 +1,21 @@
 from django.db import models
 from .categoria import Categoria
+from .tamanho import Tamanho
 
 class Produto(models.Model):
+    class Tipo(models.IntegerChoices):
+        NORMAL = 1, "Normal"
+        ESPECIAL= 2, "Especial"
+        PISTACHE = 3, "Pistache"
     nome = models.CharField(max_length=100)
     categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name='categorias', null=True, blank=True)
     descricao = models.CharField(max_length=400)
+    tipo = models.IntegerField(choices=Tipo.choices,  default=Tipo.NORMAL)
     validade = models.IntegerField(default=0,blank=True, null=True)
-    especial = models.BooleanField(default=False)
     preco = models.DecimalField(max_digits=7, decimal_places=2, default=0, null=True, blank=True)
-    tamanho = models.DecimalField(max_digits=4, decimal_places=2, default=0, null=True, blank=True)
     sabor = models.CharField(max_length=100)
+    tamanho = models.ForeignKey(Tamanho, on_delete=models.PROTECT, related_name="tamanhos", null=True, blank=True)
     
     
     def __str__(self):
-        return f'{self.nome} ({self.categoria.nome})'
+       return f'{self.nome} - {self.categoria.nome} ({self.tamanho.nome}) - R${str(self.preco).replace(".", ",")}'
---
+++ core/models/tamanho.py
@@ -0,0 +1,17 @@
+from django.db import models 
+from .categoria import Categoria
+
+class Tamanho(models.Model):
+   
+
+    nome = models.CharField(max_length=10)
+    qtdFatia = models.IntegerField(default=0)
+    massakg = models.DecimalField(max_digits=4, decimal_places=2, default=0, null=True, blank=True)
+    # preco = models.DecimalField(max_digits=7, decimal_places=2, default=0, null=True, blank=True)
+    categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, null=True, blank=True)
+    formato = models.CharField(max_length=50)
+   
+    
+    
+    def __str__(self):
+        return f'{self.nome.upper()} - ({self.categoria.nome}) - ({self.qtdFatia} fatias)'
