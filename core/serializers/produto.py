452288fe5184115d8865395956a18fddee3535df
fujulia <juliafu713@gmail.com>
2025-06-11 19:22:38 -0300

    Feat: Uploud de imagens feito #9

--- core/serializers/produto.py
+++ core/serializers/produto.py
@@ -1,22 +1,34 @@
-from rest_framework.serializers import ModelSerializer
-
-from core.models import Produto
+from rest_framework.serializers import ModelSerializer, SlugRelatedField
+from uploader.models import Image
+from uploader.serializers import ImageSerializer
+from core.models import Produto, Categoria, Tamanho
 
 
 class ProdutoListSerializer(ModelSerializer):
     class Meta:
         model = Produto
-        fields = ("id", "nome", "preco", "sabor")  
-        
+        fields = ("id", "nome", "preco", "sabor")
+
 
 class ProdutoRetrieveSerializer(ModelSerializer):
+    foto = ImageSerializer(many=True, required=False)  
     class Meta:
         model = Produto
         fields = "__all__"
-        depth = 1 
+        depth = 1
 
 
 class ProdutoSerializer(ModelSerializer):
+    foto_attachment_keys = SlugRelatedField(
+        source='foto',  
+        many=True,
+        queryset=Image.objects.all(),
+        slug_field='attachment_key',
+        required=False,
+        write_only=True,
+    )
+    foto = ImageSerializer(many=True, required=False, read_only=True)
+
     class Meta:
         model = Produto
         fields = "__all__"
