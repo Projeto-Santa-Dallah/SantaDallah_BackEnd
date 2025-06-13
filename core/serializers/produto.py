9727eb35f330c6495828124b6120b7aa60cf654b
fujulia <juliafu713@gmail.com>
2025-06-13 16:41:18 -0300

    Feat: Mudanças serializers

--- core/serializers/produto.py
+++ core/serializers/produto.py
@@ -1,3 +1,4 @@
+from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer, SlugRelatedField
 from uploader.models import Image
 from uploader.serializers import ImageSerializer
@@ -5,9 +6,16 @@ from core.models import Produto, Categoria, Tamanho
 
 
 class ProdutoListSerializer(ModelSerializer):
+    foto_url = serializers.SerializerMethodField()
+
     class Meta:
         model = Produto
-        fields = ("id", "nome", "preco", "sabor")
+        fields = ("id", "nome", "preco", "sabor", "foto_url")
+
+    def get_foto_url(self, obj):
+        if obj.foto.exists():
+            return obj.foto.first().url  # Ou o campo que tem a url da imagem
+        return None
 
 
 class ProdutoRetrieveSerializer(ModelSerializer):
