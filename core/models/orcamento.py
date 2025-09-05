43f1ff9e83dd24ec75fd9453ce1106f5b3795716
fujulia <juliafu713@gmail.com>
2025-09-05 14:20:52 -0300

    Feat: considerações feitas #5

--- core/models/orcamento.py
+++ core/models/orcamento.py
@@ -3,10 +3,11 @@ from .user import User
 
 class Orcamento(models.Model):
     usuario = models.ForeignKey(User, on_delete=models.PROTECT, related_name="orcamentos", null=True, blank=True)
-    data = models.DateField(null=False)
-    qtnPessoas = models.IntegerField(default=0)
-    local = models.CharField(max_length=100)
-    consideracoes = models.CharField(max_length=200)
+    data = models.DateField(null=True, blank=True)
+    qtnPessoas = models.IntegerField(default=0,null=True, blank=True)
+    local = models.CharField(max_length=10,null=True, blank=True)
+    bebidaAlcoolica = models.BooleanField(null=True, blank=True)
+    docinhos = models.BooleanField(null=True, blank=True)
     
     def __str__(self):
         return f'{self.usuario} ({self.data})'
