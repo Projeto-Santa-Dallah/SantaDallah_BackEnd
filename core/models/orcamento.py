497e670e4e13fe52292b11d81559e91677612684
fujulia <juliafu713@gmail.com>
2025-09-05 14:27:10 -0300

    Feat: foto add #5

--- core/models/orcamento.py
+++ core/models/orcamento.py
@@ -1,5 +1,6 @@
 from django.db import models
 from .user import User
+from uploader.models import Image
 
 class Orcamento(models.Model):
     usuario = models.ForeignKey(User, on_delete=models.PROTECT, related_name="orcamentos", null=True, blank=True)
@@ -8,6 +9,12 @@ class Orcamento(models.Model):
     local = models.CharField(max_length=10,null=True, blank=True)
     bebidaAlcoolica = models.BooleanField(null=True, blank=True)
     docinhos = models.BooleanField(null=True, blank=True)
+    foto = models.ManyToManyField(
+        Image,
+        related_name="orcamento_foto",
+        blank=True,
+        default=None,
+    )
     
     def __str__(self):
         return f'{self.usuario} ({self.data})'
