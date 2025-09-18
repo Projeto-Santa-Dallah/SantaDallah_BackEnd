7c295762f1aaa0ef42be5a6cacde336caf10a2d5
fujulia <juliafu713@gmail.com>
2025-09-18 17:27:24 -0300

    Feat: login #41

--- core/authentication.py
+++ core/authentication.py
@@ -3,12 +3,11 @@ from django.core.exceptions import ObjectDoesNotExist
 from drf_spectacular.extensions import OpenApiAuthenticationExtension
 from drf_spectacular.plumbing import build_bearer_security_scheme_object
 from passageidentity import Passage, PassageError
-
-# from passageidentity.openapi_client.models import UserInfo
 from rest_framework import authentication
 from rest_framework.exceptions import AuthenticationFailed
+from django.contrib.auth.models import Group
 
-from core.models import User
+from core.models import User  # seu modelo custom
 
 PASSAGE_APP_ID = settings.PASSAGE_APP_ID
 PASSAGE_API_KEY = settings.PASSAGE_API_KEY
@@ -16,24 +15,24 @@ psg = Passage(PASSAGE_APP_ID, PASSAGE_API_KEY)
 
 
 class TokenAuthenticationScheme(OpenApiAuthenticationExtension):
-    target_class = 'core.authentication.TokenAuthentication'
-    name = 'tokenAuth'
+    target_class = "core.authentication.TokenAuthentication"
+    name = "tokenAuth"
     match_subclasses = True
     priority = -1
 
     def get_security_definition(self, auto_schema):
         return build_bearer_security_scheme_object(
-            header_name='Authorization',
-            token_prefix='Bearer',
+            header_name="Authorization",
+            token_prefix="Bearer",
         )
 
 
 class TokenAuthentication(authentication.BaseAuthentication):
     def authenticate(self, request) -> tuple[User, None]:
-        if not request.headers.get('Authorization'):
+        if not request.headers.get("Authorization"):
             return None
 
-        token = request.headers.get('Authorization').split()[1]
+        token = request.headers.get("Authorization").split()[1]
         psg_user_id: str = self._get_user_id(token)
         user: User = self._get_or_create_user(psg_user_id)
 
@@ -42,20 +41,37 @@ class TokenAuthentication(authentication.BaseAuthentication):
     def _get_or_create_user(self, psg_user_id) -> User:
         try:
             user: User = User.objects.get(passage_id=psg_user_id)
+            psg_user = psg.user.get(psg_user_id)  # já retorna dict/objeto
         except ObjectDoesNotExist:
-            psg_user = psg.user.get(psg_user_id)
-            user: User = User.objects.create_user(
+            try:
+                psg_user = psg.user.get(psg_user_id)
+                user: User = User.objects.create_user(
                 passage_id=psg_user.id,
                 email=psg_user.email,
+                name=psg_user.email.split("@")[0]
             )
+            except PassageError as e:
+                raise AuthenticationFailed("Erro ao obter informações do usuário do Passage.") from e
 
+        self._promote_if_admin(user)
         return user
+
+    def _promote_if_admin(self, user: User) -> None:
+        if user.email in settings.ADMIN_EMAILS:
+            group, _ = Group.objects.get_or_create(name=settings.ADMIN_GROUP_NAME)
+            user.groups.add(group)
+            user.is_staff = True
+            user.is_superuser = True
+            user.save()
+        else:
+            group, _ = Group.objects.get_or_create(name=settings.CLIENTE_GROUP_NAME)
+            user.groups.add(group)
+            user.is_staff = False
+            user.is_superuser = False
+            user.save()
 
     def _get_user_id(self, token) -> str:
         try:
-            psg_user_id: str = psg.auth.validate_jwt(token)
+            return psg.auth.validate_jwt(token)
         except PassageError as e:
-            # print(e)
-            raise AuthenticationFailed(e.message) from e
-
-        return psg_user_id
+            raise AuthenticationFailed("Falha na autenticação do token.") from e
