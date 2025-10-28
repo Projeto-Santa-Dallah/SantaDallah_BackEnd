9935a196e5fdcd6c3e55f36b2ec53769c5baa2bd
fujulia <juliafu713@gmail.com>
2025-10-28 08:56:34 -0300

    Feat: Mudanças feitas #52

--- core/models/produto.py
+++ core/models/produto.py
@@ -7,27 +7,37 @@ from uploader.models import Image
 class Produto(models.Model):
     class Tipo(models.IntegerChoices):
         NORMAL = 1, "Normal"
-        ESPECIAL= 2, "Especial"
+        ESPECIAL = 2, "Especial"
         PISTACHE = 3, "Pistache"
+
     nome = models.CharField(max_length=100)
-    # categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name='categorias', null=True, blank=True)
     categoria = models.ManyToManyField(Categoria, related_name="produtos", blank=True)
     descricao = models.CharField(max_length=400)
-    tipo = models.IntegerField(choices=Tipo.choices,  default=Tipo.NORMAL)
-    validade = models.IntegerField(default=0,blank=True, null=True)
-    preco = models.DecimalField(max_digits=7, decimal_places=2, default=0, null=True, blank=True)
+    tipo = models.IntegerField(choices=Tipo.choices, default=Tipo.NORMAL)
+    validade = models.IntegerField(default=0, blank=True, null=True)
+    # preco = models.DecimalField(max_digits=7, decimal_places=2, default=0, null=True, blank=True)
     sabor = models.CharField(max_length=100)
-    tamanho = models.ForeignKey(Tamanho, on_delete=models.PROTECT, related_name="produtos", null=True, blank=True)
+    tamanhos = models.ManyToManyField(Tamanho, through='ProdutoTamanho', related_name='produtos', blank=True)
     foto = models.ManyToManyField(
         Image,
         related_name="produto_foto",
         blank=True,
         default=None,
     )
-    
-    
+
     def __str__(self):
         categorias_nomes = ", ".join([cat.nome for cat in self.categoria.all()]) if self.pk else "Sem categoria"
-        tamanho_nome = self.tamanho.nome if self.tamanho else "Sem tamanho"
-        preco_str = str(self.preco).replace(".", ",") if self.preco else "0,00"
-        return f'{self.nome} - {categorias_nomes} ({tamanho_nome}) - R${preco_str}'
+        # preco_str = str(self.preco).replace(".", ",") if self.preco else "0,00"
+        return f'{self.nome} - {categorias_nomes}'
+
+
+class ProdutoTamanho(models.Model):
+    produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
+    tamanho = models.ForeignKey(Tamanho, on_delete=models.PROTECT)
+    preco = models.DecimalField(max_digits=7, decimal_places=2)
+
+    class Meta:
+        unique_together = ('produto', 'tamanho')
+
+    def __str__(self):
+        return f"{self.produto.nome} - {self.tamanho.nome} - R${self.preco}"
